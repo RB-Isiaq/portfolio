eb3a1ab21a7951c47c2a1bae14942fe2364e0b4c
Isiaq Ridwan Bukola <isiaqridwanbukola1999@gmail.com>
2023-04-02 17:49:30 +0100

    portfolio updated

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
 (function () {
-    [...document.querySelectorAll(".control")].forEach(button => {
-        button.addEventListener("click", function() {
-            document.querySelector(".active-btn").classList.remove("active-btn");
-            this.classList.add("active-btn");
-            document.querySelector(".active").classList.remove("active");
-            document.getElementById(button.dataset.id).classList.add("active");
-        })
+  [...document.querySelectorAll(".control")].forEach((button) => {
+    button.addEventListener("click", function () {
+      document.querySelector(".active-btn").classList.remove("active-btn");
+      this.classList.add("active-btn");
+      document.querySelector(".active").classList.remove("active");
+      document.getElementById(button.dataset.id).classList.add("active");
     });
-    document.querySelector(".theme-btn").addEventListener("click", () => {
-        document.body.classList.toggle("light-mode");
-    })
+  });
+  document.querySelector(".theme-btn").addEventListener("click", () => {
+    document.body.classList.toggle("light-mode");
+  });
 })();
+
+const form = document.querySelector("form");
+const name = document.querySelector("#name");
+const email = document.querySelector("#email");
+const subject = document.querySelector("#subject");
+const message = document.querySelector("#message");
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const res = await fetch(
+    "https://portfolio-form-d941b-default-rtdb.firebaseio.com/message.json",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        name: name.value,
+        email: email.value,
+        subject: subject.value,
+        message: message.value,
+      }),
+    }
+  );
+  if (res.ok) {
+    window.alert("Sent succesfully! ✅");
+  }
+  location.reload();
+});
